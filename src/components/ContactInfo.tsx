70e2509e0ca650f18b208d95d242ab0c49e00fbc
lulucuyvers <lulu.cuyvers@madoc.be>
2025-12-05 09:21:37 +0100

    Remove figma:asset logo from ContactInfo

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,5 +1,3 @@
-import logo from 'figma:asset/8f51625c5ca59940f6672c548e557e64eed153aa.png';
-
 export function ContactInfo() {
   return (
     <div className="pl-[6px] pr-[6px] pt-8 pb-[6px] fixed bottom-0 right-0 w-1/4 flex justify-between items-end">
@@ -10,7 +8,6 @@ export function ContactInfo() {
         <p>+32 497 47 11 04</p>
         <p>info@madoc.be</p>
       </div>
-      <img src={logo} alt="Madoc logo" className="h-16" />
     </div>
   );
 }
